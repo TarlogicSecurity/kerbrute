9e0179f249ad27547fd5e13fde752e5b0c84207e
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2020-05-31 13:58:32 +0200

    Adding dynamic file writing with flush() so we can follow progress with tail -f

--- kerbrute.py
+++ kerbrute.py
@@ -229,6 +229,7 @@ class KerberosBruter:
 
             if self.out_creds_file:
                 self.out_creds_file.write("%s:%s\n" % (user, password))
+                self.out_creds_file.flush()
 
             if self.save_ticket:
                 ccache = CCache()
@@ -252,6 +253,7 @@ class KerberosBruter:
 
             if self.out_creds_file:
                 self.out_creds_file.write("%s:%s\n" % (user, password))
+                self.out_creds_file.flush()
 
     def _report_bad_user(self, user):
         with self.report_lock:
@@ -268,6 +270,7 @@ class KerberosBruter:
 
             if self.out_users_file:
                 self.out_users_file.write("%s\n" % user)
+                self.out_users_file.flush()
 
             self.good_users[user] = True
             logging.info('Valid user => %s' % user)
@@ -279,6 +282,7 @@ class KerberosBruter:
 
             if self.out_users_file:
                 self.out_users_file.write("%s\n" % user)
+                self.out_users_file.flush()
 
             self.good_users[user] = True
             logging.info('Valid user => %s [NOT PREAUTH]' % user)
